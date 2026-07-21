45f52803eb35b5f8d065cd51bae7476707aa563d
mintdk <yaritzacarmenluna89@gmail.com>
2026-07-21 16:49:12 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA324kfL3KOnz3DWtDIJ71tS8Vtn9gPUh0",
+  authDomain: "bootwo-f3ea4.firebaseapp.com",
+  projectId: "bootwo-f3ea4",
+  storageBucket: "bootwo-f3ea4.firebasestorage.app",
+  messagingSenderId: "608555122608",
+  appId: "1:608555122608:web:0ccdaf919366334927e075",
+  measurementId: "G-9ETRYVR1XS"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
