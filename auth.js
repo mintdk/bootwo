e9905bbedc8e3b8ea78a5dfbe5d3fb577f64e673
mintdk <yaritzacarmenluna89@gmail.com>
2026-07-21 16:52:57 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,28 @@
+import { auth } from "./firebase.js";
 
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try{
+
+        await signInWithEmailAndPassword(auth,email,password);
+
+        window.location.href="dashboard.html";
+
+    }catch(error){
+
+        document.getElementById("error").style.display="block";
+
+    }
+
+});
